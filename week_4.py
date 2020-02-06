276e1a102e6f74ade8ee00e65dbf66873869616c
Colm <colmocaoimh@outlook.com>
2020-02-06 16:31:43 +0000

    Week 4 homework

--- week_4.py
+++ week_4.py
@@ -4,17 +4,16 @@
 # Ask user to input any positive integer
 x = int(input("Enter any positive integer: "))
 
+# Create empty list which will contain output values
 y =[]
 
 # Create while loop which breaks once the current value is 1 
 while x > 1:
     if x % 2 == 0:
         x = x / 2
-        # print(x)
     else:
         x % 2 != 0
         x = (x * 3) + 1
-        # print(x)
     print(x)
     y.append(int(x))
 
