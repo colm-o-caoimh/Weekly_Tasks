f04a86b232103a34b56998f2e22ef97ca8315d10
Colm <colmocaoimh@outlook.com>
2020-02-27 11:57:32 +0000

    Week 6 homework updated

--- week_6.py
+++ week_6.py
@@ -12,12 +12,12 @@ def sqrt(x):
     count = 0
     # While loop iterates through count values, incrementing by 0.01 each iteration
     while x > count:
-        # Round count down to 2 decimal places as this is an approximation. Set to variable a.
+        # Round count to 2 decimal places as this is an approximation. Set to variable a.
         a = round(count, 2)
         b = round(a ** 2, 1)
         # If statement breaks when count ** 2 = user input
         if b == round(x, 1):
-            print('The square root of', x, 'is approx.', round(a, 1))
+            print('The square root of', x, 'is approx.', round(a, 2))
             break 
         # Gradually increment until square root is established
         count += 0.01
